f91117710df90f0a0af44b8efd0e9c51b0f448a7
heyrui18 <hey.rui185@gmail.com>
2026-06-21 16:49:18 +0800

    Fix ESM/CJS conflict: mark hangul-romanize as server external package

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  serverExternalPackages: ['youtube-transcript'],
+  serverExternalPackages: ['youtube-transcript', 'hangul-romanize', 'hangulx'],
 }
 
 module.exports = nextConfig
